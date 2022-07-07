8dd06682e7d5d1e2060da11cc7c320990217d6fd
Chattille <chattille@qq.com>
2022-07-07 16:13:58 +0800

    将 CSS 拆分成多个文件

--- source/conf.py
+++ source/conf.py
@@ -80,5 +80,6 @@ pygments_dark_style = 'monokai'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = ['custom.css']
+html_css_files = ['collapse.css', 'custom.css',
+                  'tabcard.css', 'translated.css']
 html_favicon = '_images/favicon.ico'
